026bdae9071e0a2e21137c49247ef0461c739bdb
Rouhis <eeturouhiainen1@gmail.com>
2023-04-16 23:55:33 +0300

    Added popupview

---
+++ EVChargeGX/Views/PopUpView.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpView.swift
+//  EVChargeGX
+//
+//  Created by iosdev on 16.4.2023.
+//
+
+import SwiftUI
+
+struct PopUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopUpView()
+    }
+}
