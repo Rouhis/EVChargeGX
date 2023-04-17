389dfbdbc09f55e01e4ae13ccd63f84aab3b7550
Rouhis <eeturouhiainen1@gmail.com>
2023-04-17 10:49:53 +0300

    addwed nav view

---
+++ EVChargeGX/Views/NavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationView.swift
+//  EVChargeGX
+//
+//  Created by iosdev on 17.4.2023.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView()
+    }
+}
