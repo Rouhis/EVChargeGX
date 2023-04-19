6f8d382dae46dc72d75e1234fc4ba7f4b7174b12
Tuomas01 <tuomastheikkila@hotmail.fi>
2023-04-19 11:22:54 +0300

    add NavigatorView file

---
+++ EVChargeGX/Views/NavigatorView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigatorView.swift
+//  EVChargeGX
+//
+//  Created by iosdev on 4/19/23.
+//
+
+import SwiftUI
+
+struct NavigatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigatorView()
+    }
+}
